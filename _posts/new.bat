8bc82f66081c44c65fcb7ec3cd4ec8619d7b2181
yaowj <yaowj@ychcapital.com>
2016-07-18 11:44:12 +0800

    0718

---
+++ _posts/new.bat
@@ -0,0 +1,7 @@
+echo.--->> %date:~0,4%-%date:~5,2%-%date:~8,2%-%1%.md
+echo.layout: post>> %date:~0,4%-%date:~5,2%-%date:~8,2%-%1%.md
+echo.title: "">> %date:~0,4%-%date:~5,2%-%date:~8,2%-%1%.md
+echo.date: %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,8% +0800>> %date:~0,4%-%date:~5,2%-%date:~8,2%-%1%.md
+echo.categories: tag>> %date:~0,4%-%date:~5,2%-%date:~8,2%-%1%.md
+echo.--->> %date:~0,4%-%date:~5,2%-%date:~8,2%-%1%.md
+echo.
